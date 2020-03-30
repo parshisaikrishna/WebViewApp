5f87d68aa73da7ca0ae8a43898e57567ac861fe0
parshi sai krishna <sai.k@principleacs.com>
2020-03-30 23:23:31 +0530

    webView sample

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private  WebView myWebView;
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         myWebView = findViewById(R.id.web_view);
+       myWebView.setWebViewClient(new MyWebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        openURL();
+
+//        myWebView.loadUrl("https://email.principleacs.com/");
+
+    }
+    private void openURL() {
+        myWebView.loadUrl("https://www.worldometers.info/coronavirus/country/india/");
+        myWebView.requestFocus();
+    }
+}
